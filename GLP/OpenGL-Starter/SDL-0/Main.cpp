8404681c6355e29fa7d6f5eb375fafb7836df2c7
Evann <evann.jacquot@artfx.fr>
2024-11-04 15:33:34 +0100

    [add] orange triangle

--- GLP/OpenGL-Starter/SDL-0/Main.cpp
+++ GLP/OpenGL-Starter/SDL-0/Main.cpp
@@ -4,6 +4,31 @@
 using namespace std;
 //#define GLEW_STATIC
 
+float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+};
+
+const char* vertexShaderSource = "#version 330 core\n"
+"in vec3 pos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);\n"
+"}\0";
+
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+
+GLuint VAO, VBO;
+unsigned int vertexShader, fragmentShader;
+unsigned int shaderProgram;
+
 int main(int argc, char* argv[])
 {
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
@@ -33,6 +58,79 @@ int main(int argc, char* argv[])
 		cout << "Glew initialized successfully\n";
 	}
 
-	cin.get();
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &VAO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); 
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
+	glEnableVertexAttribArray(0);
+
+	//vertex shader 
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+
+	//fragment shader 
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	//shaderpProgram
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glUseProgram(shaderProgram);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	// Set the viewing frame through which we will see the objects
+	glViewport(0, 0, width, height);
+	//Put the color you want here for the background
+	glClearColor(0.0f, 0.0f, 0.2f, 1.0f);
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	bool isRunning = true;
+	while (isRunning) {
+		// Inputs
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+			case SDL_QUIT:
+				isRunning = false;
+				break;
+			default:
+				break;
+			}
+		}
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the screen
+
+
+		// Draw function
+		glUseProgram(shaderProgram);
+
+		glBindVertexArray(VAO);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+
+		SDL_GL_SwapWindow(Window); // Swapbuffer
+	}
+	// Quit
+	SDL_DestroyWindow(Window);
+	SDL_GL_DeleteContext(Context);
+
 	return 0;
 }
+
